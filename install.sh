3c7eee08a07250d3a0e2c20289daba80e7993521
neoice <neoice@neoice.net>
2013-05-29 20:59:48 -0700

    terrible SECRET_KEY generator

--- install.sh
+++ install.sh
@@ -3,3 +3,5 @@
 virtualenv .
 source bin/activate
 pip install -r requirements.txt
+
+sed -i -e "s/SECRET_KEY = ''/SECRET_KEY = '`dd if=/dev/urandom bs=1 count=32 | md5sum`'/g" app1/app1/settings.py
